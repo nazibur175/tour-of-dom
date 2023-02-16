025a1be024a1fb70e1290cfa39ebd9eb1fa95fa4
nazibur-175 <nazibur175@gmail.com>
2023-02-16 15:33:43 +0600

    first commit

---
+++ js/dom.js
@@ -0,0 +1,5 @@
+//console.log('Hello I am from javascript');
+const liCollection = document.getElementsByTagName('li');
+//console.log(liCollection);
+for(const li of liCollection)
+ console.log(li.innerText);
